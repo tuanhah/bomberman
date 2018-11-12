cc9d91c2684ab041c85e7d20a01c9c9391509a3c
tuanhah <tuantsukuyomi@gmail.com>
2018-11-12 21:14:14 +0700

    fix bug

--- src/uet/oop/bomberman/entities/character/enemy/ai/AIMedium.java
+++ src/uet/oop/bomberman/entities/character/enemy/ai/AIMedium.java
@@ -2,6 +2,7 @@ package uet.oop.bomberman.entities.character.enemy.ai;
 
 import uet.oop.bomberman.entities.character.Bomber;
 import uet.oop.bomberman.entities.character.enemy.Enemy;
+import uet.oop.bomberman.entities.character.enemy.Oneal;
 
 public class AIMedium extends AI {
 	Bomber _bomber;
@@ -14,7 +15,14 @@ public class AIMedium extends AI {
 
 	@Override
 	public int calculateDirection() {
+		return 1;
+	}
+	public int calculateDirectionOneal(Oneal oneal) {
 		// TODO: cài đặt thuật toán tìm đường đi
+		int x_bomber =_bomber.getXTile();
+		int y_bomber =_bomber.getYTile();
+		int x_oneal = oneal.getXTile();
+		int y_oneal = oneal.getYTile();
 		return 1;
 	}
 
